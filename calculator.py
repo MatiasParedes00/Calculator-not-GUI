f49254be348e19b094a457311fd256f7755ead18
matias paredes <matiasagustinparedes00@gmail.com>
2020-07-02 12:54:15 -0300

    Added some basic functionality

--- calculator.py
+++ calculator.py
@@ -1,2 +1,27 @@
-import functions
+import functions as fun
+import decorators as deco
+import sys
 
+while True:
+    deco.decorator1 ("Enter the number:")
+    print ("1) One number operation.\n")
+    print ("2) Two numbers operation.\n")
+    deco.decorator2 ("3) Quit")
+    inp = input(": ")
+    if inp == "1":
+        print ("test")
+    elif inp == "2":
+        print ("test")
+    elif inp == "3":
+        while True:
+            inp = input("Are you sure? (Y/N): ")
+            if inp == "Y" or inp == "y":
+                sys.exit(0)
+            elif inp == "N" or inp == "n":
+                break
+            else:
+                print ("Incorrect input.\n")
+                continue
+    else:
+        print ("Incorrect input.\n")
+        continue
